721ef1b5222b4def89bd1a08b9ce0b3a801a722b
galobponce <personal.galo.bello@gmail.com>
2023-06-20 23:47:46 -0300

    Add pizzas list

---
+++ src/components/Menu/pizzas.js
@@ -0,0 +1,45 @@
+export const pizzas = [
+  {
+    name: "Salciccia",
+    image_name: "salciccia",
+    description:
+      "Tomato sauce, Mozarella, Tomato, Homemade sausage, Garlic, Basil",
+  },
+  {
+    name: "Gamberi",
+    image_name: "gamberi",
+    description: "Tomato sauce, Mozarella, Shrimps, Feta cheese, Olives, Basil",
+  },
+  {
+    name: "Pepe",
+    image_name: "pepe",
+    description: "Tomato sauce, Mozarella, Chilli flakes, Olives, Basil",
+  },
+  {
+    name: "Disgustoso",
+    image_name: "disgustoso",
+    description: "Tomato sauce, Bacon, Pineapple, Olives, Basil",
+  },
+  {
+    name: "Colorato",
+    image_name: "colorato",
+    description: "Tomato sauce, Mozarella, Onion, Pepper, Champignons, Basil",
+  },
+  {
+    name: "Pomodoro",
+    image_name: "pomodoro",
+    description: "Tomato sauce, Mozarella, Tomato, Onion, Feta Cheese, Chilli",
+  },
+  {
+    name: "Crema",
+    image_name: "crema",
+    description:
+      "White sauce, Mozarella, Shrimps, Salmon, Pineapple, Olives, Basil",
+  },
+  {
+    name: "Carne",
+    image_name: "carne",
+    description:
+      "Tomato sauce, Mozarella, Homemade sausage, Bacon, Garlic, Pepper, Chilli",
+  },
+]
